4c97fad8605b125e27d91c129e10e66d559a7275
ravi2krishna <ravi2krishna@gmail.com>
2025-06-10 08:21:25 +0530

    strings are immutable

---
+++ string_buffer_builder/Test.java
@@ -0,0 +1,10 @@
+package string_buffer_builder;
+
+public class Test {
+    public static void main(String[] args) {
+        String str1 = "hello ";
+        System.out.println("Before Modification "+str1);
+        System.out.println(str1.concat("ravi"));
+        System.out.println("After Modification "+str1);
+    }
+}
